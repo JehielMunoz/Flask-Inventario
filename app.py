f87b5c1597739468cdf05ce85a194063ed2d54a5
JehielMunoz <jehiel.munoz2015@alu.uct.cl>
2021-06-17 07:00:10 -0400

    Primeras pruebas de lectura y escritura.

--- app.py
+++ app.py
@@ -1,10 +1,82 @@
-from flask import Flask, render_template
+import firebase_admin
+from firebase_admin import db,credentials
+from flask import Flask, render_template, request
+import pyrebase
+from werkzeug.utils import redirect
 
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-    return render_template('index.html')
+cred = credentials.Certificate('key.json')
 
-if __name__ == "__main__":
+firebase_admin.initialize_app(cred, {
+    'databaseURL': 'https://proyectoi-invedu-default-rtdb.firebaseio.com/'
+    })
+database = db.reference('/')
+
+
+@app.route('/', methods=['GET','POST'])
+def index():
+    # if request.method == 'POST' :
+    # name = request.form.get("form")
+    # db.child("19176913-9").push(name)
+    # te = []
+    provs = database.child("proveedores").get()
+    if provs is None:
+        return render_template("index.html")
+    else:
+        return render_template("index.html", t=provs)
+
+
+@app.route('/addItem.html', methods=['GET','POST'])
+def addItem():
+    if request.method == 'POST':
+        especie = request.form["especie"]
+        estado = request.form["estado"]
+        precio_unitario = request.form["precio_unitario"]
+        precio_total = request.form["precio_total"]
+        fecha_recepcion = request.form["fecha_recepcion"]
+        numero_factura = request.form["numero_factura"]
+        rut_proveedor = request.form["rut_proveedor"]
+        centro_de_costo = request.form["centro_de_costo"]
+        ubicacion_actual = request.form["ubicacion_actual"]
+        observaciones = request.form["observaciones"]
+        codigo_barra = request.form["codigo_barra"]
+
+        item = {
+            "codigo_correlativo": 1003,
+            "especie":  especie,
+            "estado":   estado,
+            "precio_unitario":  precio_unitario,
+            "precio_total": precio_total,
+            "fecha_recepcion":  fecha_recepcion,
+            "numero_factura":   numero_factura,
+            "rut_proveedor":    rut_proveedor,
+            "centro_de_costo":  centro_de_costo,
+            "ubicacion_actual": ubicacion_actual,
+            "observaciones":    observaciones,
+            "codigo_barra": codigo_barra
+        }
+        try:
+            database = db.reference('/')
+            database = database.child("data").child("especies")
+            database.push({"1003": item})
+            print(item)
+            # database.child("data").child("especies").child(str(last_code)).push(item)
+            # database.child(last_code).patch(item)
+            return redirect("/")
+        except:
+            return render_template("addItem.html", message="Something wrong happened")
+    return render_template("addItem.html")
+if __name__ == '__main__':
     app.run(debug=True)
+"""
+:
+    last_code = len(database.child("data").child("especies").get())
+    # maxId = len(lastCod)
+    # lastCod = database.child("data").child("especies").child(str(maxId)).get()
+    return render_template("addItem.html", cod=last_code)
+
+
+@app.route('/addItem.html', methods=['GET','POST'])
+def createItem():
+"""
--- database/conn.py
+++ database/conn.py
@@ -1,19 +1,49 @@
-import firebase_admin
+import firebase_admin, os
 from firebase_admin import credentials,db
 
-cred = credentials.Certificate('proyectoi-invedu-firebase-adminsdk-nplni-3aa203bb29.json')
+cred = credentials.Certificate('')
+cred = credentials.Certificate('authKey.json')
 
 firebase_admin.initialize_app(cred, {
   'databaseURL' : 'https://proyectoi-invedu-default-rtdb.firebaseio.com/'
 })
 
-ref = db.reference('/').child("proveedores")
-nProv = "19176913-2"
+
+def home():
+  result = db.reference("/").get('/proveedores', None)
+  return str(result)
+
+nProv = "3333333-3"
+def setRef():
+  ref = db.reference('/').child("proveedores")
+  return ref
+def getAllProv(ref):
+  return print(ref.get())
+
+def putNewProv(ref,nProv):
+  ref.put({
+    nProv:
+      {
+        'Nombre': 'test1',
+        'apellido': 'apptest'
+      }
+  })
+
+"""for i in ref:
+  rut = ref.child(i).child("rut").get()
+  razon_social = str(ref.child(i).child("razonSocial").get())
+  print(rut, razon_social)"""
+
+"""nProv = "3333333-3"
 new_prov = ref.child(nProv)
-ref.push({
+rut = new_prov.child("rut")
+razon_social = str(new_prov.child("razonSocial").get())
+print("Rut:" + rut)
+print("Razon social:" + razon_social)"""
+"""ref.push({
   '19176913-2' :
     {
       'Nombre':'test1',
       'apellido':'apptest'
     }
-})
+})"""
